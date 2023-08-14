2aa4863ad994232978df284f4d3e80b836369a96
wachopuffs <perezchalanda.21@outlook.com>
2023-08-14 15:17:21 -0600

    cambios

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var google : any ;
+
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
@@ -7,9 +9,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPage implements OnInit {
 
+  map = null;
+
   constructor() { }
 
   ngOnInit() {
+    this.loadMap();
   }
 
+  loadMap() {
+    const mapEle: HTMLElement | null = document.getElementById('map');
+    
+    if (mapEle !== null) {
+      // Check if geolocation is available
+      if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          const myLatLng = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+          
+          this.map = new google.maps.Map(mapEle, {
+            center: myLatLng,
+            zoom: 12
+          });
+    
+          google.maps.event.addListenerOnce(this.map, 'idle', () => {
+            mapEle.classList.add('show-map');
+          });
+        }, (error) => {
+          console.error('Error getting location:', error);
+          // Fallback to a default location if getting user location fails
+          const defaultLatLng = { lat: 4.658383846282959, lng: -74.09394073486328 };
+          this.map = new google.maps.Map(mapEle, {
+            center: defaultLatLng,
+            zoom: 12
+          });
+    
+          google.maps.event.addListenerOnce(this.map, 'idle', () => {
+            mapEle.classList.add('show-map');
+          });
+        });
+      } else {
+        console.error('Geolocation is not available.');
+        // Fallback to a default location if geolocation is not available
+        const defaultLatLng = { lat: 4.658383846282959, lng: -74.09394073486328 };
+        this.map = new google.maps.Map(mapEle, {
+          center: defaultLatLng,
+          zoom: 12
+        });
+  
+        google.maps.event.addListenerOnce(this.map, 'idle', () => {
+          mapEle.classList.add('show-map');
+        });
+      }
+    } else {
+      console.error("Element with ID 'map' not found.");
+    }
+  }
 }
